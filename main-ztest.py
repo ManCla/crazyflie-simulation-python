eae3c642c549a258ea68b6b31afca38722b3c8b5
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-14 14:44:02 +0100

    improved heatmap labeling and added more tests

--- main-ztest.py
+++ main-ztest.py
@@ -16,7 +16,7 @@ z_test_directory = 'z-test'
 duration  = 20     # duration of flight
 
 frequencies = [0, .25, .5, 1, 2, 3, 4, 8]
-amplitudes  = [.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 5, 6, 7, 10, 20]
+amplitudes  = [.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5, 5.5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 20]
 
 if __name__ == "__main__":
 
@@ -59,19 +59,30 @@ if __name__ == "__main__":
 				storeObj.open(file_path, file_name.split('/')[-1],True)
 				# show flight performance
 				print("   Flight performance: {}".format(storeObj.compute_z_error()))
-			error[j,i] = storeObj.compute_z_error()
-			saturation[j,i] = storeObj.motors_saturated()
+				error[j,i] = storeObj.compute_z_error()
+				saturation[j,i] = storeObj.motors_saturated()
 
 	# plot error heat map
 	fig, ax = plt.subplots(1, 2)
 
+	# ticks and ticks labels
+	x_ticks           = range(len(frequencies))
+	x_ticks_labels    = list(map(str,frequencies))
+	x_ticks_labels[0] = 'step'
+	y_ticks           = range(len(amplitudes))
+	y_ticks_labels    = list(map(str,amplitudes))
+
 	ax[0].imshow(error, cmap='hot', interpolation='nearest',origin='lower')
 	ax[0].set_title('error')
-	plt.xticks(range(len(frequencies)),list(map(str,frequencies)))
-	plt.yticks(range(len(amplitudes)),list(map(str,amplitudes)))
+	ax[0].set_xticks(x_ticks)
+	ax[0].set_xticklabels(x_ticks_labels)
+	ax[0].set_yticks(y_ticks)
+	ax[0].set_yticklabels(y_ticks_labels)
 	ax[1].imshow(saturation, cmap='hot', interpolation='nearest',origin='lower')
 	ax[1].set_title('saturation')
-	plt.xticks(range(len(frequencies)),list(map(str,frequencies)))
-	plt.yticks(range(len(amplitudes)),list(map(str,amplitudes)))
+	ax[1].set_xticks(x_ticks)
+	ax[1].set_xticklabels(x_ticks_labels)
+	ax[1].set_yticks(y_ticks)
+	ax[1].set_yticklabels(y_ticks_labels)
 	plt.show()
-	code.interact(local=locals())
+	# code.interact(local=locals())
