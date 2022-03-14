87c1d59100f5ce2b349724168d74956e9265d5e1
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-14 17:39:26 +0100

    removed non setpoint normalized error

--- plot-ztests.py
+++ plot-ztests.py
@@ -3,40 +3,46 @@ import numpy as np
 
 path = 'flightdata/z-aggregated/'
 
+exclude_step_responses = True
+
 if __name__ == "__main__":
 
     frequencies = np.genfromtxt(path+'frequencies.csv', delimiter=',')
     amplitudes  = np.genfromtxt(path+'amplitudes.csv', delimiter=',') 
-    z_error_cumulative     = np.genfromtxt(path+'z_error_cumulative.csv', delimiter=',')
     z_error_rel_cumulative = np.genfromtxt(path+'z_error_rel_cumulative.csv', delimiter=',')
     motors_saturated       = np.genfromtxt(path+'motors_saturated.csv', delimiter=',')
     hit_ground             = np.genfromtxt(path+'hit_ground.csv', delimiter=',')
 
+    if exclude_step_responses:
+        num_freqs = len(frequencies)
+        frequencies = frequencies[1:num_freqs]
+        z_error_rel_cumulative = z_error_rel_cumulative[:,1:num_freqs]
+        motors_saturated = motors_saturated[:,1:num_freqs]
+        hit_ground = hit_ground[:,1:num_freqs]
+
     # generate plotting objects
-    fig, ax = plt.subplots(1, 3)
+    fig, ax = plt.subplots(1, 2)
 
     # ticks and ticks labels
     x_ticks           = range(len(frequencies))
     x_ticks_labels    = list(map(str,frequencies))
-    x_ticks_labels[0] = 'step'
+    if not(exclude_step_responses):
+        x_ticks_labels[0] = 'step'
     y_ticks           = range(len(amplitudes))
     y_ticks_labels    = list(map(str,amplitudes))
     plt.setp(ax, xticks=x_ticks, xticklabels=x_ticks_labels,\
                  yticks=y_ticks, yticklabels=y_ticks_labels)
 
-    ax[0].imshow(z_error_cumulative, cmap='jet', interpolation='nearest',origin='lower')
-    ax[0].set_title('cumulative error')
-    ax[1].imshow(z_error_rel_cumulative, cmap='jet', interpolation='nearest',origin='lower')
-    ax[1].set_title('cumulative error relative (to sp)')
-    ax[2].imshow(motors_saturated, cmap='jet', interpolation='nearest',origin='lower')
-    ax[2].set_title('total saturation time')
+    ax[0].imshow(z_error_rel_cumulative, cmap='jet', interpolation='nearest',origin='lower')
+    ax[0].set_title('cumulative error relative (to sp)')
+    ax[1].imshow(motors_saturated, cmap='jet', interpolation='nearest',origin='lower')
+    ax[1].set_title('total saturation time')
 
     # add values inside cells
     for i in range(len(frequencies)):
         for j in range(len(amplitudes)):
-            ax[0].text(i,j,'%d'%(z_error_cumulative[j,i]),    ha='center',va='center')
-            ax[1].text(i,j,'%d'%(z_error_rel_cumulative[j,i]),ha='center',va='center')
-            ax[2].text(i,j,'%d'%(motors_saturated[j,i]),      ha='center',va='center')
+            ax[0].text(i,j,'%d'%(z_error_rel_cumulative[j,i]/z_error_rel_cumulative[0,0]),ha='center',va='center')
+            ax[1].text(i,j,'%.2f'%(motors_saturated[j,i]),      ha='center',va='center')
 
     plt.show()
     # code.interact(local=locals())
