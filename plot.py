3a6bdf61d8811914365a403cb0e44b01cf793058
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-03-30 14:35:20 +0200

    no need for experiment name when opening data file

--- plot.py
+++ plot.py
@@ -7,17 +7,15 @@ if __name__ == "__main__":
 
     if len(sys.argv) == 2: # if data file is given
         file_location   = sys.argv[1]
-        experiment_name = file_location.split('/')[-1]
     else : # otherwise take latest test flight
-        dir_content = os.listdir("flightdata")
+        dir_content = os.listdir(fdh.data_directory)
         files_only = list(filter(lambda x:os.path.isfile('flightdata/'+x), dir_content))
         files_list = list(map(lambda x:datetime.strptime(x,fdh.date_format), files_only))
         files_list.sort()
-        experiment_name = files_list[-1].strftime(fdh.date_format)
-        file_location = fdh.data_directory+"/"+ experiment_name
+        file_location = fdh.data_directory+"/"+ files_list[-1].strftime(fdh.date_format)
 
     data_storage = fdh()
-    data_storage.open(file_location, experiment_name)
+    data_storage.open(file_location)
     print('* read data with length: \033[33m' + str(data_storage.trace_length) + '\033[0m')
 
     data_storage.show_all()
