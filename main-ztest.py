24e57d308aaff7454f87bb091d51d4224c49c062
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-04-19 09:33:37 +0200

    no need to perform tests that are known to fail

--- main-ztest.py
+++ main-ztest.py
@@ -14,9 +14,9 @@ import numpy as np
 z_test_directory    = 'z-test'
 z_results_directory = 'z-aggregated'
 
-duration  = 30     # duration of flight
+duration  = 50     # duration of flight
 
-frequencies = [0, .25, .5, .75, .875, 1, 1.125, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.5, 4, 4.5]
+frequencies = [ .25, .5, .75, .875, 1, 1.125, 1.25, 1.5, 1.75, 2, 2.25, 2.5, 2.75, 3, 3.5, 4, 4.5]
 amplitudes  = [.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 4.75, 5, 5.25, 5.5, 5.75, 6,\
                 7, 8, 8.5, 9, 10, 11, 12, 13]
 
@@ -40,39 +40,40 @@ if __name__ == "__main__":
 				base = amp
 			else :
 				base = amp+1
-			ref = zTestCase(base, amp, freq)
+			if i<=9 or j<=7:
+				ref = zTestCase(base, amp, freq)
 
-			# retrieve location and name of test output file
-			file_name = ref.trajectoryType+'-'+\
-			            str(ref.base)+'-'+\
-			            str(ref.amplitude)+'-'+\
-			            str(ref.omega)
-			file_path = FlightDataHandler.data_directory+'/'+\
-			            z_test_directory+'/'+\
-			            file_name
+				# retrieve location and name of test output file
+				file_name = ref.trajectoryType+'-'+\
+				            str(ref.base)+'-'+\
+				            str(ref.amplitude)+'-'+\
+				            str(ref.omega)
+				file_path = FlightDataHandler.data_directory+'/'+\
+				            z_test_directory+'/'+\
+				            file_name
 
-			# If test has not been executed, do it otherwise, print performance
-			if not(exists(file_path)):
-				print(" * Executing Test {}".format(file_name))
-				sim        = cfSimulation()          # initialize simulation object
-				start_test = time.perf_counter()
-				storeObj   = sim.run(ref, duration)  # actual test execution
-				end_test   = time.perf_counter()
-				print(" >> {} took {} seconds".format(file_name, str(end_test-start_test)))
-				storeObj.save(z_test_directory+'/'+file_name) # store simulation results
-				storeObj.unwrap(storeObj)
-			else :
-				print(" * Test {} already executed".format(file_name))
-				storeObj = FlightDataHandler()
-				storeObj.open(file_path,True)
-				# show flight performance
-			z_avg_error[j,i]      = storeObj.compute_z_avg_error_abs()
-			z_avg_error_rel[j,i]  = storeObj.compute_z_avg_error_rel()
-			z_max_error[j,i]      = storeObj.compute_z_max_error()
-			motors_saturated[j,i] = storeObj.motors_saturated()
-			hit_ground[j,i]       = storeObj.hit_ground()
-			z_filtering[j,i]      = storeObj.compute_z_filtering()
-			behaviour[j,i]        = storeObj.compute_behaviour()
+				# If test has not been executed, do it otherwise, print performance
+				if not(exists(file_path)):
+					print(" * Executing Test {}".format(file_name))
+					sim        = cfSimulation()          # initialize simulation object
+					start_test = time.perf_counter()
+					storeObj   = sim.run(ref, duration)  # actual test execution
+					end_test   = time.perf_counter()
+					print(" >> {} took {} seconds".format(file_name, str(end_test-start_test)))
+					storeObj.save(z_test_directory+'/'+file_name) # store simulation results
+					storeObj.unwrap(storeObj)
+				else :
+					print(" * Test {} already executed".format(file_name))
+					storeObj = FlightDataHandler()
+					storeObj.open(file_path,True)
+					# show flight performance
+				z_avg_error[j,i]      = storeObj.compute_z_avg_error_abs()
+				z_avg_error_rel[j,i]  = storeObj.compute_z_avg_error_rel()
+				z_max_error[j,i]      = storeObj.compute_z_max_error()
+				motors_saturated[j,i] = storeObj.motors_saturated()
+				hit_ground[j,i]       = storeObj.hit_ground()
+				z_filtering[j,i]      = storeObj.compute_z_filtering()
+				behaviour[j,i]        = storeObj.compute_behaviour()
 
 	## write results to csv
 	out_path = FlightDataHandler.data_directory+'/'+z_results_directory+'/'
