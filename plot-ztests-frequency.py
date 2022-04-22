2313ff8d574b8229aba80fdd78817ec02bb662ce
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-04-22 17:55:01 +0200

    use plotting parameters from fdh class

--- plot-ztests-frequency.py
+++ plot-ztests-frequency.py
@@ -11,18 +11,6 @@ z_test_directory    = 'z-test'
 # script parameters
 plot_only_peaks = True
 
-chosen_grid_linewidth = 0.3
-chosen_grid_linestyle = '--'
-chosen_grid_color = 'gray'
-
-# colour palette for behaviour coding
-bh_palette = np.array([[255,255,255],  # black: bh_undefined
-                       [  0,255,  0],  # green: bh_good_tracking
-                       [  0,  0,255],  # blue : bh_filtering
-                       [  0,255,255],  # azure: bh_good_tracking_extra
-                       [255,  0,  0],  # red  : bh_sat_no_tracking
-                       [  0,  0,  0]]) # dark : bh_something_wrong
-
 if __name__ == "__main__":
 
     ######################
@@ -37,7 +25,7 @@ if __name__ == "__main__":
     fig1, ax = plt.subplots(1, 1)
     plt.subplots_adjust(wspace=0.2, hspace=1)
     plt.setp(ax,xlabel=x_label,ylabel=y_label)
-    ax.grid(color=chosen_grid_color, linestyle=chosen_grid_linestyle, linewidth=chosen_grid_linewidth)
+    ax.grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
 
     ##########################################
     ### Iterate over Test Results and Plot ###
@@ -46,8 +34,8 @@ if __name__ == "__main__":
     directory = fdh.data_directory+'/'+z_test_directory
     dir_content = os.listdir(directory)
     # filter out directories and look only at files
-    dir_content = list(filter(lambda x:os.path.isfile(directory+x), dir_content))
-    
+    dir_content = list(filter(lambda x:os.path.isfile(directory+'/'+x), dir_content))
+
     for file in dir_content:
         # file opening and data extraction
         file_path = directory+'/'+file
@@ -56,7 +44,7 @@ if __name__ == "__main__":
         data_storage.analyse_z() # needed to get the frequency spectrum
 
         # actual plotting for each test
-        bh_color=bh_palette[data_storage.behaviour]/255
+        bh_color=fdh.bh_palette[data_storage.behaviour]/255
         if plot_only_peaks :
             peaks, _ = ss.find_peaks(data_storage.z_ref_fft)
             ax.plot(np.array(data_storage.z_fft_freq)[peaks],\
