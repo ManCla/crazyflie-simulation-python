04f1405be3a87ba35b1ed5429b93648807ccbbb0
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-04-25 17:13:03 +0200

    some more tests

--- main-ztest-set.py
+++ main-ztest-set.py
@@ -39,12 +39,12 @@ shapes = zShapes.shapes
 ### Time Coefficients
 # for each time coefficient tests will be performed
 # using the coefficient to shrink a dilate time
-time_speeds = [1,2]
+time_speeds = [2,3,4]
 
 ### Amplitude Coefficients
 # for each amplitude coefficient tests will be performed
 # using the coefficient to scale the amplitude of the signal
-amplitudes  = [0.25,0.5]
+amplitudes  = [1,2,3]
 
 ####################
 ### TEST WARM UP ###
