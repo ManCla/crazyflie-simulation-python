e4a45e347053dadc972f74a4dedabba6f9ed61f0
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-04-29 12:43:55 +0200

    added separated plots for different types of tests

--- plot-ztests-frequency.py
+++ plot-ztests-frequency.py
@@ -20,11 +20,21 @@ if __name__ == "__main__":
     x_label           = "Frequency [Hz]"
     y_label           = "Amplitude [m]"
     
-    # generate plotting objects
-    fig1, ax = plt.subplots(1, 1)
+    ### generate plotting objects
+    # figure with separated different types of tests
+    fig1, ax  = plt.subplots(4, 1)
     plt.subplots_adjust(wspace=0.2, hspace=1)
     plt.setp(ax,xlabel=x_label,ylabel=y_label)
-    ax.grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
+    # TODO --- do not hardcode this but rather take this from the z-test-set file
+    ax[0].grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
+    ax[1].grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
+    ax[2].grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
+    ax[3].grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
+
+    # figure with all tests together
+    fig2, ax2 = plt.subplots(1, 1)
+    plt.setp(ax2,xlabel=x_label,ylabel=y_label)
+    ax2.grid(color=fdh.chosen_grid_color, linestyle=fdh.chosen_grid_linestyle, linewidth=fdh.chosen_grid_linewidth)
 
     ##########################################
     ### Iterate over Test Results and Plot ###
@@ -41,19 +51,46 @@ if __name__ == "__main__":
         data_storage = fdh()
         data_storage.open(file_path, silent=True)
 
+        if data_storage.test=="steps" :
+            index = 0
+        elif data_storage.test=="triangular" :
+            index = 1
+        elif data_storage.test=="trapezoidal" :
+            index = 2
+        elif data_storage.test=="sinus" :
+            index = 3
+
         # actual plotting for each test
         bh_color=fdh.bh_palette[data_storage.get_behaviour()]/255
         if plot_only_peaks :
-            ax.plot(data_storage.get_z_fft_freq_peaks(),\
-                    data_storage.get_z_ref_fft_peaks(),\
-                    "x",color=bh_color)
-            ax.plot(data_storage.get_z_fft_freq(),\
-                    data_storage.get_z_ref_fft(),\
-                    color=bh_color)
+            ax[index].plot(data_storage.get_z_fft_freq_peaks(),\
+                           data_storage.get_z_ref_fft_peaks(),\
+                           "x",color=bh_color)
+            ax2.plot(data_storage.get_z_fft_freq_peaks(),\
+                     data_storage.get_z_ref_fft_peaks(),\
+                     "x",color=bh_color)
         else :
-            ax.plot(data_storage.get_z_fft_freq(),\
-                    data_storage.get_z_ref_fft(),\
-                    color=bh_color, linewidth=1)
-        ax.set_xlim([0, 5]) # spectrum above 1Hz seems to be basically zero
-
+            ax[index].plot(data_storage.get_z_fft_freq(),\
+                           data_storage.get_z_ref_fft(),\
+                           color=bh_color, linewidth=1)
+            ax2.plot(data_storage.get_z_fft_freq(),\
+                     data_storage.get_z_ref_fft(),\
+                     color=bh_color, linewidth=1)
+    x_max = 3
+    y_max = 120000
+    ax[0].set_xlim([0, x_max]) # spectrum above 1Hz seems to be basically zero
+    ax[1].set_xlim([0, x_max]) # spectrum above 1Hz seems to be basically zero
+    ax[2].set_xlim([0, x_max]) # spectrum above 1Hz seems to be basically zero
+    ax[3].set_xlim([0, x_max]) # spectrum above 1Hz seems to be basically zero
+    ax[0].set_ylim([0, y_max])
+    ax[1].set_ylim([0, y_max])
+    ax[2].set_ylim([0, y_max])
+    ax[3].set_ylim([0, y_max])
+    ax[0].title.set_text("steps")
+    ax[1].title.set_text("triangular")
+    ax[2].title.set_text("trapezoidal")
+    ax[3].title.set_text("sinus")
+    ax2.set_xlim([0, x_max]) # spectrum above 1Hz seems to be basically zero
+    ax2.set_ylim([0, y_max])
+    ax2.title.set_text("All tests together")
     plt.show()
