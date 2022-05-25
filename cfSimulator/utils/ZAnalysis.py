319c4bb787c0cb0760e9f9f367bc63d32e46ea11
Claudio Mandrioli <claudio.mandrioli@control.lth.se>
2022-05-25 13:56:32 +0200

    removed time domain analysis and better detection of motor sat and hit ground in separate function

--- cfSimulator/utils/ZAnalysis.py
+++ cfSimulator/utils/ZAnalysis.py
@@ -75,8 +75,6 @@ class ZAnalysis(FlightDataHandler):
         stain_mot_sat = False # if you want to mark behaviour also according to motor saturation
         thrust_min = 20000 # saturation limits of motors
         thrust_max = 65535 #
-        motors_saturated_threshold = int(0.05*self.trace_length)
-        # TODO --- add staining of tests that hit the ground
         ### END PARAMETERS
 
         # apply fft to a given number of periods
@@ -133,12 +131,9 @@ class ZAnalysis(FlightDataHandler):
 
         ### BEHAVIOUR DETECTION ###
         self.freq_analysis_behaviour = [self.bh_undefined] * len(ref_peaks_indexes)
-        # detect motor saturation
-        mot_sat = sum([(x==thrust_min or x==thrust_max) for x in self.u[0,settle:self.trace_length]])\
-                  >motors_saturated_threshold
         # iterate over frequency of peaks of position spectrum
-        ## TO address non periodic signals you just want to iterate over each point of the frequency spectrum.
-        ## TODO try that
+        ## to address non periodic signals you just want to iterate over each point of the frequency spectrum.
+        ## TODO: try that
         for pp_idx in pos_peaks_indexes :
             # look for same peak in reference spectrum peaks
             find_ref_peak = [abs(x-pp_idx)<freq_diff_tolerance for x in ref_peaks_indexes]
@@ -169,103 +164,34 @@ class ZAnalysis(FlightDataHandler):
                     idx = np.where(ref_peaks_indexes==rp_idx)[0][0]
                     self.freq_analysis_behaviour[idx] = self.bh_filtering
 
-    def analyse_z(self):
-        err_rel     = 0  # error normalized over setpoint
-        err_abs_cum = 0 
-        mot_sat_tot = 0  # total time motors are saturated [# of time steps]
-        hit_ground  = 0  # total time spent "hitting the ground" [# of time steps]
-        max_error   = 0
-
+    def analyse_z_sat_and_ground(self):
         ### PARAMETERS -- TODO should be defined elsewhere
         dt = 0.001         # sampling time in seconds
         thrust_min = 20000 # saturation limits of motors
         thrust_max = 65535 # 
         settle     = int(5/dt) #
-        # time in percentage of test trace length above which the drone is considered to have hit the saturations
-        motors_saturated_threshold = 0.001 
-        # deviation from expected gain of 1 that is accepted for good reference tracking
-        filtering_threshold = 0.2
-        avg_error_rel_threshold = 0.5
         ### END PARAMETERS
 
-        # remove the warm up (defined by settle variable)
-        time_range = range(settle,self.trace_length)
-        
-        # TIME DOM. ANALYSIS: iterate over time and perform actual analysis
-        for i in time_range:
-            abs_error = np.abs(self.set_pt[2,i] - self.pos[2,:][i])
-            if abs_error>max_error :
-                max_error = abs_error
-            err_rel = err_rel + abs_error/self.set_pt[2,i]
-            err_abs_cum = err_abs_cum + abs_error
-            mot_sat_tot = mot_sat_tot + \
-                          (self.u[0,i]<thrust_min+1 or\
-                           self.u[0,i]>thrust_max-1)
-            hit_ground  = hit_ground + (self.pos[2,:][i]<0.01)
-        # finalize analysis
-        self.z_avg_error_abs        = err_abs_cum/(self.trace_length-settle)
-        self.z_avg_error_rel        = err_rel/(self.trace_length-settle)
-        self.z_max_error            = max_error
-        # compute saturation and ground time as percentage of total test time
-        self.motors_saturated_time  = mot_sat_tot/(self.trace_length-settle)
-        self.hit_ground_time        = hit_ground/(self.trace_length-settle)
+        self.motors_saturated_percentage = sum([(x==thrust_min or x==thrust_max) for x in self.u[0,settle:self.trace_length]])\
+                                           /(self.trace_length-settle)
+        self.hit_ground_percentage = sum( x<0.01 for x in self.pos[2,settle:self.trace_length] )\
+                                     /(self.trace_length-settle)
+        print("motor sat"+str(self.motors_saturated_percentage))
+        print("hit ground"+str(self.hit_ground_percentage))
 
-        self.freq_behaviour_z() # needed only to make reference and output spectra available
-        # check for filtering of reference's frequency with largest component
-        index_input_freq = np.where(self.z_ref_fft== np.amax(self.z_ref_fft))
-        self.z_filtering =  self.z_pos_fft[index_input_freq[0][0]]\
-                            /self.z_ref_fft[index_input_freq[0][0]]
-
-        # define behavioural region on the base of: 
-        #  - hitting saturations
-        #  - good reference tracking
-        #  - filtering reference
-        self.time_analysis_behaviour = self.bh_undefined
-        if self.motors_saturated_time<motors_saturated_threshold : 
-            # we have not saturated
-            if abs(self.z_filtering-1)>filtering_threshold :
-                self.time_analysis_behaviour = self.bh_filtering
-            else :
-                if self.z_avg_error_rel>avg_error_rel_threshold :
-                    self.time_analysis_behaviour = self.bh_something_wrong
-                else :
-                    self.time_analysis_behaviour = self.bh_good_tracking
-        else :
-            # we have saturated
-            if self.z_avg_error_rel<avg_error_rel_threshold :
-                self.time_analysis_behaviour = self.bh_good_tracking_extra
-            else :
-                self.time_analysis_behaviour = self.bh_sat_no_tracking
-
-    def get_z_avg_error_abs(self):
-        if not(hasattr(self, "z_avg_error_abs")):
-            self.analyse_z()
-        return self.z_avg_error_abs
-
-    def get_z_avg_error_rel(self):
-        if not(hasattr(self, "z_avg_error_rel")):
-            self.analyse_z()
-        return self.z_avg_error_rel
-
-    def get_z_max_error(self):
-        if not(hasattr(self, "z_max_error")):
-            self.analyse_z()
-        return self.z_max_error
+    #####################
+    ### GET FUNCTIONS ###
+    #####################
 
     def get_motors_saturated(self):
-        if not(hasattr(self, "motors_saturated_time")):
-            self.analyse_z()
-        return self.motors_saturated_time
+        if not(hasattr(self, "motors_saturated_percentage")):
+            self.analyse_z_sat_and_ground()
+        return self.motors_saturated_percentage
 
     def get_hit_ground(self):
-        if not(hasattr(self, "hit_ground_time")):
-            self.analyse_z()
-        return self.hit_ground_time
-
-    def get_z_filtering(self):
-        if not(hasattr(self, "z_filtering")):
-            self.analyse_z()
-        return self.z_filtering
+        if not(hasattr(self, "hit_ground_percentage")):
+            self.analyse_z_sat_and_ground()
+        return self.hit_ground_percentage
 
     def get_behaviour(self):
         if not(hasattr(self, "behaviour")):
@@ -274,25 +200,25 @@ class ZAnalysis(FlightDataHandler):
 
     def get_z_fft_freq(self):
         if not(hasattr(self, "z_fft_freq")):
-            self.analyse_z()
+            self.freq_behaviour_z()
         return self.z_fft_freq
 
     def get_z_pos_fft(self):
         if not(hasattr(self, "z_pos_fft")):
-            self.analyse_z()
+            self.freq_behaviour_z()
         return self.z_pos_fft
 
     def get_z_ref_fft(self):
         if not(hasattr(self, "z_ref_fft")):
-            self.analyse_z()
+            self.freq_behaviour_z()
         return self.z_ref_fft
 
     def get_z_fft_freq_peaks(self):
         if not(hasattr(self, "z_ref_freq_peaks")):
-            self.analyse_z()
+            self.freq_behaviour_z()
         return self.z_ref_freq_peaks
 
     def get_z_ref_fft_peaks(self):
         if not(hasattr(self, "z_ref_amp_peaks")):
-            self.analyse_z()
+            self.freq_behaviour_z()
         return self.z_ref_amp_peaks
